973cac002bfa90271b92f7be71575fc5ec3a6da8
kopom <kopom@SHIBEJ>
2024-11-20 09:35:49 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Zmienne
+int add1 = 0;
+int add2 = 0;
+
+// Queston
+Console.WriteLine("Wpisz liczbę!");
+string userInput = Console.ReadLine();
+
+add1 = int.Parse(userInput);
+
+
+//Program
+Console.WriteLine("Podaj 2 liczbę!");
+userInput = Console.ReadLine();
+add2 = int.Parse(userInput);
+
+int sum = add1 + add2;
+
+Console.WriteLine($"Twoj wynik to {sum}");
+Console.ReadKey();
